33cc6d61541682b4f0b4b57fb99dc781530fedd3
Eugeniy-Makarevich <eugeniy.makarevich@live.com>
2023-01-16 19:33:08 +0200

    scrap_hot function added

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+#import modules
 import praw
-import os
+import pprint
 
+#initialize PRAW and get subreddit
 reddit = praw.Reddit("BOT")
-print(reddit.read_only)
-for submission in reddit.subreddit("test").hot(limit=10):
-    print(submission.title)
+subreddit = reddit.subreddit("subredditdrama")
+
+def scrape_hot(n_last_submissions):
+    for submission in subreddit.hot(limit = n_last_submissions):
+        yield {
+              'title' : submission.title,
+               'upvote' : submission.score,
+               'author' : submission.author,
+               'num_comments' : submission.num_comments,
+               'upvote_ratio' : submission.upvote_ratio,
+               'created'      : submission.created_utc
+               }
+               
+               
+               
+               
+               
+         
+
