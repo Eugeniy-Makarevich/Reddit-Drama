305cf7345dbfac80a3e6a71707a2ec6e6fa2078e
Eugeniy-Makarevich <eugeniy.makarevich@live.com>
2023-01-18 21:49:58 +0200

    scraping to csv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #import modules
-import praw, json
+import praw, csv
 
 #initialize PRAW and get subreddit
 reddit = praw.Reddit("BOT")
@@ -8,22 +8,24 @@ subreddit = reddit.subreddit("subredditdrama")
 # function returns generator with info about submissions in subreddit - new. 
 def scrape_hot(n_last_submissions):
     for submission in subreddit.new(limit = n_last_submissions):
-        yield {
-              'title' : submission.title,
-               'upvote' : submission.score,
-               'author' : submission.author.name if submission.author 
-                                                 else 'deleted',
-               'num_comments' : submission.num_comments,
-               'upvote_ratio' : submission.upvote_ratio,
-               'created'      : submission.created_utc
-               }
-        
+        yield [ submission.title,
+                submission.score,
+                submission.author.name if submission.author 
+                                        else 'deleted',
+                submission.num_comments,
+                submission.upvote_ratio,
+                submission.created_utc ]
+                         
 # function appends row to file
 def append_record(record):
-    with open('reddit_drama.txt', 'a') as f:
-        json.dump(record, f)
-        f.write('\n') 
-              
+    with open('reddit_drama.csv', 'a',encoding = 'utf-8',newline = '') as f:
+        writer = csv.writer(f,delimiter = ';')
+        writer.writerow(record)
+        
+# write header   
+header = ['title','upvotes','author','comments','upvote_ratio','created'] 
+append_record(header)
+           
 # write file             
 for row in scrape_hot(1000):
        append_record(row)           
